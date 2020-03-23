481854bb8e54be8c7053ac5103542fcae7bd4870
Francesco31Ott <francy.3131@gmail.com>
2020-03-23 09:48:58 +0100

    MQTT for environmental stations

--- FirstAssignment/VirtualEnvironmentalStations/MQTT/PublisherClient.py
+++ FirstAssignment/VirtualEnvironmentalStations/MQTT/PublisherClient.py
@@ -1,82 +1,65 @@
-#!/usr/bin/python
-
-############################################ 
-# PROBLEM STATEMENT:
-# This program will publish test mqtt messages using the AWS IOT hub
-# 
-# To test this program you have to run first its companinon aws_iot_sub.py
-# that will subscribe and show all the messages sent by this program
-#
-############################################
-
-############################################
-# STEPS:
-#
-# 1. Sign in to AWS Amazon > Services > AWS IoT > Settings > copy Endpoint
-#    This is your awshost
-# 
-# 2. Change following things in the below program:
-#    a. awshost   (from step 1)
-#    b. clientId  (Thing_Name)
-#    c. thingName (Thing_Name)
-#    d. caPath    (root-CA_certificate_Name)
-#    e. certPath  (<Thing_Name>.cert.pem)
-#    f. keyPath   (<Thing_Name>.private.key)
-# 
-# 3. Paste aws_iot_pub.py & aws_iot_sub.py python scripts in folder where all unzipped aws files are kept. 
-# 4. Provide Executable permition for both the python scripts.
-# 5. Run aws_iot_sub.py script
-# 6. Run this aws_iot_pub.py python script
-#
-############################################
+#!/usr/bin/env python
 
 # importing libraries
-import paho.mqtt.client as paho
+import paho.mqtt.client as mqtt
 import os
 import socket
 import ssl
 from time import sleep
-from random import uniform
+from random import randint
  
 connflag = False
  
-def on_connect(client, userdata, flags, rc):                # func for making connection
+def on_connect(client, userdata, flags, rc):                        # function for making connection
     global connflag
     print("Connected to AWS")
     connflag = True
     print("Connection returned result: " + str(rc) )
  
-def on_message(client, userdata, msg):                      # Func for Sending msg
+def on_message(client, userdata, msg):                              # function for ending msg
     print(msg.topic+" "+str(msg.payload))
  
 #def on_log(client, userdata, level, buf):
 #    print(msg.topic+" "+str(msg.payload))
  
-mqttc = paho.Client()                                       # mqttc object
-mqttc.on_connect = on_connect                               # assign on_connect func
-mqttc.on_message = on_message                               # assign on_message func
+mqttc = mqtt.Client()                                               # mqttc object
+mqttc.on_connect = on_connect                                       # assign on_connect function
+mqttc.on_message = on_message                                       # assign on_message function
 #mqttc.on_log = on_log
 
 #### Change following parameters #### 
-awshost = "a3um1mnv6jt2hg-ats.iot.us-east-1.amazonaws.com"  # Endpoint
-awsport = 8883                                              # Port no.   
-clientId = "myThing"                                        # Thing_Name
-thingName = "myThing"                                       # Thing_Name
-caPath = "../certs/root-CA.crt"                                      # Root_CA_Certificate_Name
-certPath = "../certs/certificate.pem.crt"                            # <Thing_Name>.cert.pem
-keyPath = "../certs/private.pem.key"                          # <Thing_Name>.private.key
+awshost = "a3um1mnv6jt2hg-ats.iot.us-east-1.amazonaws.com"          # Endpoint
+awsport = 8883                                                      # Port no.   
+clientId = "myThing"                                                # Thing_Name
+thingName = "myThing"                                               # Thing_Name
+caPath = "../../certs/root-CA.crt"                                  # Root_CA_Certificate_Name.crt
+certPath = "../../certs/certificate.pem.crt"                        # <Thing_Name>.cert.pem.crt
+keyPath = "../../certs/private.pem.key"                             # <Thing_Name>.private.pem.key
  
 mqttc.tls_set(caPath, certfile=certPath, keyfile=keyPath, cert_reqs=ssl.CERT_REQUIRED, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)  # pass parameters
  
-mqttc.connect(awshost, awsport, keepalive=60)               # connect to aws server
+mqttc.connect(awshost, awsport, keepalive=60)                       # connect to aws server
  
-mqttc.loop_start()                                          # Start the loop
+mqttc.loop_start()                                                  # Start the loop
  
-while 1==1:
+while(1):
     sleep(5)
     if connflag == True:
-        tempreading = uniform(20.0,25.0)                        # Generating Temperature Readings 
-        mqttc.publish("temperature", tempreading, qos=1)        # topic: temperature # Publishing Temperature values
-        print("msg sent: temperature " + "%.2f" % tempreading ) # Print sent temperature msg on console
+        temp = randint(-50,51)                                      # Generating Multiple Readings
+        hum = randint(0, 101)
+        wind_dir = randint(0, 361)
+        wind_int = randint(0, 101)
+        rain = randint(0, 51)                                   
+        mqttc.publish("temperature", temp, qos=1)                   # topic: <TopicName> # Publishing Topics values
+        mqttc.publish("humidity", hum, qos=1)        
+        mqttc.publish("windDirection", wind_dir, qos=1)        
+        mqttc.publish("windIntensity", wind_int, qos=1)        
+        mqttc.publish("rainHeight", rain, qos=1)        
+        
+        print("Message sent: temperature ",temp," Celsius")         # Print sent <TopicName> msg on console
+        print("Message sent: humidity ",hum," %")
+        print("Message sent: windDirection ",wind_dir," Degrees")
+        print("Message sent: windIntensity ",wind_int," m/s")
+        print("Message sent: rainHeight ",rain," mm/h\n")
     else:
         print("waiting for connection...")
