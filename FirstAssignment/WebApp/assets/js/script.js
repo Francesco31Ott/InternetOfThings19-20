fd8b945c10ea27a10704ae9286fab01f1489bb55
Francesco31Ott <francy.3131@gmail.com>
2020-03-25 23:51:05 +0100

    Fixed some issues

--- FirstAssignment/WebApp/assets/js/script.js
+++ FirstAssignment/WebApp/assets/js/script.js
@@ -1,50 +1,22 @@
+// Configuration with AWS Cognito Identity Credentials.
 AWS.config.region = "us-east-1";
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: "us-east-1:f79208a2-a26f-4c2b-be6d-ec367432dded"
 });
 
-/*
-  // The endpoint should point to the local or remote computer where DynamoDB (downloadable) is running.
-  endpoint: 'https://a3um1mnv6jt2hg-ats.iot.us-east-1.amazonaws.com',
-  /*
-    accessKeyId and secretAccessKey defaults can be used while using the downloadable version of DynamoDB. 
-    For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-  *//*
-accessKeyId: "ASIARWJDDJX22HK2QSMC",
-secretAccessKey: "1ANToxWyQy0G2W1nN3vfjLSfeHajLf72whxR4rSa"
-});*/
-/* 
-   Uncomment the following code to configure Amazon Cognito and make sure to 
-   remove the endpoint, accessKeyId and secretAccessKey specified in the code above. 
-   Make sure Cognito is available in the DynamoDB web service region (specified above).
-   Finally, modify the IdentityPoolId and the RoleArn with your own.
-*/
-/*
-  // Inizializza il provider di credenziali Amazon Cognito
-CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-    getApplicationContext(),
-    "us-east-1:f79208a2-a26f-4c2b-be6d-ec367432dded", // ID pool di identità
-    Regions.US_EAST_1 // Regione
-);
-*/
-
-/*
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-IdentityPoolId: "us-east-1:f79208a2-a26f-4c2b-be6d-ec367432dded",
-RoleArn: "arn:aws:iam::116574604789:role/service-role/IoTDynamo"
-});
-*/
-// set up connection with dynamodb
+// Set up connection with DynamoDB.
 var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-// list of station in the db
+// List of stations in the Database.
 var stations = new Set();
 
-// function that get the last value from the table.
-// since that the oldest value is the first of the list,
-// I have to reverse this list and take the first tuple
+// Function that get the last value from the table.
+// Since that the oldest value is the first of the list,
+// I have to reverse this list and take the first tuple.
 function getLatestValue() {
+
+    // Parameters for the scan of the DB.
     var params = {
         ExpressionAttributeNames: {
             "#id": "id"
@@ -55,6 +27,8 @@ function getLatestValue() {
 
     docClient.scan(params, onScan);
 
+    // Function that scans the table EnvironmentalStationDB,
+    // then adds the ID of the stations in the list 'stations'
     function onScan(err, data) {
         if (err) {
             console.log("Error", err);
@@ -65,11 +39,6 @@ function getLatestValue() {
             stations = Array.from(stations);
             stations.sort();
             console.log("success", stations);
-
-            var currentDate = new Date();
-            currentDate.setHours(currentDate.getHours() - 1);
-
-            console.log("ciao " + currentDate.getTime());
 
             stations.forEach(function (id) {
                 //Parameters of the query.
@@ -88,37 +57,46 @@ function getLatestValue() {
                 console.log("prova", id);
 
 
-                //Queries the data from the selected station.
+                //Query from each station.
                 docClient.query(params, function (err, data) {
                     if (err) {
                         console.log("Error", err);
                     } else {
-                        //console.log("success", data.Items.reverse()[0]);
-                        //Exploiting the fact that the data is already ordered.
+
+                        // In the variable 'latest' there will be the
+                        // last value inserted in the DB.
+                        // Since that the list is ordered from the oldest to
+                        // the newest value, I have to reverse it.
                         var latest = data.Items.reverse()[0];
-                        console.log("success", latest);
-                        var c;
+                        console.log("success");
 
-
-                        //insert latest value for each sensor
+                        // Insert latest value for each sensor.
                         document.getElementById('latest-value1').innerHTML = latest.temperature + ' °C';
                         document.getElementById('latest-value2').innerHTML = latest.humidity + '%';
                         document.getElementById('latest-value3').innerHTML = latest.windDirection + '°';
                         document.getElementById('latest-value4').innerHTML = latest.windIntensity + ' m/s';
                         document.getElementById('latest-value5').innerHTML = latest.rainHeight + ' mm/h';
 
-                        //change update on the cards
+                        var c;
+                        // Change update on the cards.
                         for (c = 1; c < 6; c++) {
                             document.getElementById('update' + c).innerHTML = '<i class="material-icons">update</i> Just Updated by Station ' + id;
                         }
 
-                        //insert latest tuple for each station
-                        var sizeList = Object.keys(data.Items).length;
-                        console.log(sizeList);
+                        // Compute the size of the table EnvironmentalStationDB
+                        // (this variable serves for the next step)
+                        //var sizeList = Object.keys(data.Items).length;
+                        //console.log(sizeList);
+
+                        // In the variable 'actual' there will be, for
+                        // each iteration of the cycle, all the tuples
+                        // from the newest to the oldest.
                         var actual = data.Items;
-                        for (c = 0; c < sizeList; c++) {
-                            document.getElementById('station' + id).innerHTML += '<tr><td>' + actual[c].datetime + '</td><td>' + actual[c].temperature + '</td><td>' + actual[c].humidity + '</td><td>'
-                                + actual[c].windDirection + '</td><td>' + actual[c].windIntensity + '</td><td>' + actual[c].rainHeight + '</td></tr> ';
+
+                        // Insert the latest 5 tuples for each station.
+                        for (c = 0; c < 5; c++) {
+                            document.getElementById('station' + id).innerHTML += '<tr><td>' + actual[c].datetime + '</td><td>' + actual[c].temperature + '</td><td>' + actual[c].humidity +
+                                '</td><td>' + actual[c].windDirection + '</td><td>' + actual[c].windIntensity + '</td><td>' + actual[c].rainHeight + '</td></tr> ';
                         }
                     }
 
@@ -128,7 +106,7 @@ function getLatestValue() {
     };
 }
 
-// function that computes the time one hour ago
+// Function that returns the one hour ago from now.
 function computeDiff(tupleDate) {
     var now = new Date();
     var year = now.getFullYear();
@@ -148,9 +126,9 @@ function computeDiff(tupleDate) {
     return hours;
 }
 
-// function that returns the actual time
+// Function that returns the current time.
 // (it serves for the 'LastHour' query)
-function getTimeNow() {
+function getDateTime() {
     var now = new Date();
     var year = now.getFullYear();
     var month = now.getMonth() + 1;
@@ -175,44 +153,54 @@ function getTimeNow() {
         second = '0' + second;
     }
 
-    var currentDateTime = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
-    var datetimeOneHour = year + '-' + month + '-' + day + ' ' + (hour - 1) + ':' + minute + ':' + second;
-    var dateTimeLast = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + (second - 5);
-    return current;
-}
-/*function insertData(i) {
-    console.log("Ecco lo script");
-    document.getElementById('latest-value1').innerHTML = i + " °C";
-    document.getElementById('latest-value2').innerHTML = i + "%";
-    document.getElementById('latest-value3').innerHTML = i + "°";
-    document.getElementById('latest-value4').innerHTML = i + " m/s";
-    document.getElementById('latest-value5').innerHTML = i + " mm/h";
- 
-    // sleep time expects milliseconds
-    function sleep(time) {
-        return new Promise((resolve) => setTimeout(resolve, time));
-    }
-}*/
+    var dateTimeOneHour = year + '-' + month + '-' + day + ' ' + (hour - 1) + ':' + minute + ':' + second;
 
+    return dateTimeOneHour;
+}
+
+// Function that returns all the tuples inserted
+// at most one hour ago. For each sensor will
+// be filled its own table.
 function getLastHourData(sensor) {
-    var dateTime = computeDiff();
+
+    // Variable that contains the time, one hour ago.
+    var lastHour = getDateTime();
+
+    // Parameters for the scan of the DB.
     var params = {
         TableName: "EnvironmentalStationDB",
-        ProjectionExpression: "id, dtime",
-        KeyConditionExpression: "#dtime between " + dateTime + " and #dtime",
-        FilterExpression: sensor + " = :sensor",
+        ProjectionExpression: "#dtime, #id, " + sensor,
+        FilterExpression: "#dtime >= :lastHour",
+        ExpressionAttributeNames: {
+            "#id": "id",
+            "#dtime": "datetime"
+
+        },
         ExpressionAttributeValues: {
-            ":hour": 1,
-            ":sensor": sensor
+            ":lastHour": lastHour
         }
     };
+    docClient.scan(params, onScan);
 
-    docClient.query(params, function (err, data) {
+    function onScan(err, data) {
         if (err) {
             console.log("Error", err);
         } else {
-            document.getElementById('id' + sensor).innerHTML += '<tr><td>' + '2020-03-25 10:56:37' + '</td><td>' + latest.id + '</td><td>' + latest.sensor + '</td></tr> ';
-
+            var c;
+            var items = data.Items;
+            var sizeList = Object.keys(items).length;
+            for (c = 0; c < sizeList; c++) {
+                if (sensor === "temperature")
+                    document.getElementById('id' + sensor).innerHTML += '<tr><td>' + items[c].datetime + '</td><td>' + items[c].id + '</td><td class="text-primary">' + items[c].temperature + '</td></tr> ';
+                else if (sensor === "humidity")
+                    document.getElementById('id' + sensor).innerHTML += '<tr><td>' + items[c].datetime + '</td><td>' + items[c].id + '</td><td class="text-primary">' + items[c].humidity + '</td></tr> ';
+                else if (sensor === "windDirection")
+                    document.getElementById('id' + sensor).innerHTML += '<tr><td>' + items[c].datetime + '</td><td>' + items[c].id + '</td><td class="text-primary">' + items[c].windDirection + '</td></tr> ';
+                else if (sensor === "windIntensity")
+                    document.getElementById('id' + sensor).innerHTML += '<tr><td>' + items[c].datetime + '</td><td>' + items[c].id + '</td><td class="text-primary">' + items[c].windIntensity + '</td></tr> ';
+                else if (sensor === "rainHeight")
+                    document.getElementById('id' + sensor).innerHTML += '<tr><td>' + items[c].datetime + '</td><td>' + items[c].id + '</td><td class="text-primary">' + items[c].rainHeight + '</td></tr> ';
+            }
         }
-    });
+    };
 }
